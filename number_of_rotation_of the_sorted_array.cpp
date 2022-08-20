9effcde837856ef7aae27d85671f42cb167b9828
Vivek <vivekkmishra1301@gmail.com>
2022-08-21 00:15:20 +0530

    to calculate the number of rotation of the sorted array

---
+++ number_of_rotation_of the_sorted_array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int arrayRotateCheck(int *input, int size)
+{
+    // Write your code here
+    int min = input[0];
+    int minind = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (min > input[i])
+        {
+            min = input[i];
+            minind = i;
+        }
+    }
+    return minind;
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int size;
+        cin >> size;
+        int *input = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            cin >> input[i];
+        }
+
+        cout << arrayRotateCheck(input, size) << endl;
+        delete[] input;
+    }
+
+    return 0;
+}
