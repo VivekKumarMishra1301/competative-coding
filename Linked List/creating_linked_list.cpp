88ff8477ac607bbd8968b891a84aabefa04d048b
Vivek <vivekkmishra1301@gmail.com>
2022-10-02 19:32:39 +0530

    creating linked list statically and dynamically and printing the linked list

---
+++ Linked List/creating_linked_list.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    // Statically
+    Node n1(1);
+    Node *head = &n1;
+
+    Node n2(2);
+
+    n1.next = &n2;
+
+    cout << (*head).data << " ";
+    head = (*head).next;
+    cout << (*head).data << " ";
+}
