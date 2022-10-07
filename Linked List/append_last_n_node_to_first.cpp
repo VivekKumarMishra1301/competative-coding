504c11f94f24582371ed343e4f290fc9bda11ac2
Vivek <vivekkmishra1301@gmail.com>
2022-10-08 00:17:58 +0530

    linked list imp que

---
+++ Linked List/append_last_n_node_to_first.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+Node *appendLastNToFirst(Node *head, int n)
+{
+    // Write your code here
+    if (head == NULL || n == 0)
+    {
+        return head;
+    }
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    temp = head;
+    int diff = count - n;
+    Node *newHead;
+    for (int i = 1; i < diff; i++)
+    {
+        temp = temp->next;
+    }
+    newHead = temp->next;
+    temp->next = NULL;
+    Node *search = newHead;
+    while (search->next != NULL)
+    {
+        search = search->next;
+    }
+    search->next = head;
+    head = newHead;
+    return head;
+}
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1)
+    {
+        Node *newnode = new Node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Node *head = takeinput();
+        int n;
+        cin >> n;
+        head = appendLastNToFirst(head, n);
+        print(head);
+    }
+    return 0;
+}
---
+++ Linked List/find_node_recursively.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+int findNode(Node *head, int n)
+{
+    // Write your code here.
+    if (head == NULL)
+    {
+        return -1;
+    }
+    if (head->data == n)
+    {
+        return 0;
+    }
+    int c = findNode(head->next, n);
+    if (c == -1)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1 + c;
+    }
+}
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Node *head = takeinput();
+        int val;
+        cin >> val;
+        cout << findNode(head, val) << endl;
+    }
+}
