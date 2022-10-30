d437e7e047f866bfbe5d647e2be472f8eda23b73
Vivek <vivekkmishra1301@gmail.com>
2022-10-31 01:04:07 +0530

    medium array question

--- Leetcode Problems/Array_single_number_II.cpp
+++ Leetcode Problems/Array_single_number_II.cpp
@@ -1,1 +1,17 @@
-i < nums.size() - 1
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0;i < nums.size();i++) {
+            if (i < nums.size() - 1 && nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+            else if (i == nums.size() - 1) {
+                return nums[nums.size() - 1];
+            }
+            if (i < nums.size() - 2)
+                i += 2;
+        }
+        return nums[0];
+    }
+};
