b827b922b8ce571cd38203decb871cf924631f21
Vivek <vivekkmishra1301@gmail.com>
2022-12-25 21:08:46 +0530

    minimum numbers used sum of whose squares is equal to number

---
+++ Dynamic Programming/Memoization_min_count.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findNum(int n, vector<int> &dp)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    int ans = INT_MAX;
+    int i = 1;
+    while (i * i <= n)
+    {
+        ans = min(ans, 1 + findNum(n - i * i, dp));
+        i++;
+    }
+    return dp[n] = ans;
+}
+
+int minCount(int n)
+{
+    // Write your code here
+
+    vector<int> dp(n + 1, -1);
+    return findNum(n, dp);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << minCount(n);
+}
