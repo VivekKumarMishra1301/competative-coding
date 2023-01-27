fc286a320f0d97bea9b0bd541a30d3ede72404a4
Vivek <vivekkmishra1301@gmail.com>
2023-01-27 23:36:42 +0530

    Problem of the day

---
+++ Leetcode Problems/Tries_Concat_words.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    unordered_map<string, int> meraMap;
+    bool canConcat(string word)
+    {
+        int n = word.size();
+        for (int i = 0; i < n; i++)
+        {
+            string pref = word.substr(0, i + 1);
+            string suff = word.substr(i + 1, n);
+            if (meraMap.count(pref) != 0 && (meraMap.count(suff) != 0 || canConcat(suff)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    vector<string> findAllConcatenatedWordsInADict(vector<string> &words)
+    {
+        vector<string> hold;
+        for (auto i : words)
+        {
+            meraMap[i]++;
+        }
+
+        for (auto i : words)
+        {
+            if (canConcat(i))
+            {
+                hold.push_back(i);
+            }
+        }
+        return hold;
+    }
+};
