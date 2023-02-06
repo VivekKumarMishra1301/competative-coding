2210c2b8da22c9dd7c13469b1da8cea123fb01c7
Vivek <vivekkmishra1301@gmail.com>
2023-02-06 22:23:12 +0530

    Number of Longest Increasing Subsequences very Imporatnt problem of the DP

---
+++ Leetcode Problems/Dynamic_Programming_Number_of_Longest_Increasing_subsequences.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+public:
+    int findNumberOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        vector<int> counter(n, 1);
+        int hold = 0;
+        // return find(arr,n-1,memo);
+        dp[0] = 1;
+        counter[0] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    int ans = 1 + dp[j];
+                    if (dp[i] < ans)
+                    {
+                        dp[i] = ans;
+                        counter[i] = counter[j];
+                    }
+                    else if (dp[i] == ans)
+                    {
+                        counter[i] += counter[j];
+                    }
+                }
+            }
+        }
+        // sort(dp.begin(),dp.end());
+        int best = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (best < dp[i])
+            {
+                best = dp[i];
+            }
+            cout << dp[i] << ":" << counter[i] << " ";
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (dp[i] == best)
+            {
+                hold += counter[i];
+            }
+        }
+
+        return hold;
+    }
+};
