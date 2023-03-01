6af0983333b1edc5e8bec39e1083f2fb1c14b257
Vivek <vivekkmishra1301@gmail.com>
2023-03-01 19:10:08 +0530

    sort the array

---
+++ Leetcode Problems/Sorting_Sort_An_array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> sortArray(vector<int> &nums)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            pq.push(nums[i]);
+        }
+        int i = 0;
+        while (!pq.empty())
+        {
+            nums[i++] = pq.top();
+            pq.pop();
+        }
+        return nums;
+    }
+};
