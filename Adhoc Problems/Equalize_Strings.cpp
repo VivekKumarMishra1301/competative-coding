be099c7ac0b9cc790cd9bf51f35ebe7b8d8d91eb
Vivek <vivekkmishra1301@gmail.com>
2023-04-21 20:01:25 +0530

    Adhoc Problems think logically

---
+++ Adhoc Problems/Equalize_Strings.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long int
+using namespace std;
+void precal()
+{
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    string t;
+    cin >> s >> t;
+    int mincost = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != t[i])
+        {
+            if (i + 1 < n && s[i + 1] != s[i] && s[i + 1] != t[i + 1])
+            {
+                swap(s[i], s[i + 1]);
+                mincost++;
+            }
+            else
+            {
+                mincost++;
+            }
+        }
+    }
+    cout << mincost << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout << setprecision(10);
+    precal();
+    int t = 1;
+    // cin>>t;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+}
