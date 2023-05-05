6bb97004b9e64af145600bc9e47b35961fe80bfa
Vivek <vivekkmishra1301@gmail.com>
2023-05-05 12:18:49 +0530

    remain changes

---
+++ Leetcode Problems/Sliding_window_Maximum_vowel_in_a_substring_of_length_k.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+public:
+    bool isvowel(char c)
+    {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int maxVowels(string s, int k)
+    {
+        map<char, int>;
+        int st = 0;
+        int en = 0;
+        int maxi = 0;
+        while (en < s.size())
+        {
+            meraMap[s[en]]++;
+            if ((en - st + 1) == k)
+            {
+                int su = 0;
+                for (auto i : meraMap)
+                {
+                    if (isvowel(i.first))
+                    {
+                        su += i.second;
+                    }
+                }
+                cout << su << " ";
+                maxi = max(maxi, su);
+                meraMap[s[st++]]--;
+            }
+            en++;
+        }
+        if (en - st == k)
+        {
+            int su = 0;
+            for (auto i : meraMap)
+            {
+                if (isvowel(i.first))
+                {
+                    su += i.second;
+                }
+            }
+            maxi = max(maxi, su);
+        }
+
+        return maxi;
+    }
+};
