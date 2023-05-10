484694a2872db0bab0f5da67a351dc3355d9cf8a
Vivek <vivekkmishra1301@gmail.com>
2023-05-11 01:00:32 +0530

    use 3d dp to implement and think for the buy in 3rd dimension if you bought then next when you will buy that is in the lese part if the stock was not bought then buy it and maintain the 3rd dimension variable by reducing it

---
+++ Leetcode Problems/Dynamic_Programming_Best_Time_to_Buy_And_Sell_Stocks_III.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int prof(int ind, int op, vector<int> &arr, vector<vector<vector<int>>> &dp, int tr)
+    {
+        if (tr == 0)
+        {
+            return 0;
+        }
+        if (ind == arr.size())
+        {
+            return 0;
+        }
+
+        if (dp[ind][op][tr] != -1)
+        {
+            return dp[ind][op][tr];
+        }
+
+        int profit;
+        if (op)
+        {
+            profit = max((-arr[ind] + prof(ind + 1, 0, arr, dp, tr)), prof(ind + 1, 1, arr, dp, tr));
+        }
+        else
+        {
+            profit = max((arr[ind] + prof(ind + 1, 1, arr, dp, tr - 1)), prof(ind + 1, 0, arr, dp, tr));
+        }
+        return dp[ind][op][tr] = profit;
+    }
+
+    int maxProfit(vector<int> &prices)
+    {
+        vector<vector<vector<int>>> dp(
+            prices.size() + 1, vector<vector<int>>(3, vector<int>(3, -1)));
+        return prof(0, 1, prices, dp, 2);
+    }
+};
