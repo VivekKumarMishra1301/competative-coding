c7386cbac903bc2a79466ce8f917b27edcbb3006
Vivek <vivekkmishra1301@gmail.com>
2023-05-28 20:22:00 +0530

    sort the array and insert length at last index and 0 at first index and to calculate length substract from last to first and do it recursivrly for each index by partitioning from the given index at cuts array

---
+++ Leetcode Problems/Dynamic_Programming_Minimum_cost_to_cut_A_stick.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int find(int i, int j, vector<int> &cuts, vector<vector<int>> &dp)
+    {
+        if (i > j)
+        {
+            return 0;
+        }
+
+        if (dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        int mini = INT_MAX;
+        for (int ind = i; ind <= j; ind++)
+        {
+            mini = min(mini, cuts[j + 1] - cuts[i - 1] + find(ind + 1, j, cuts, dp) + find(i, ind - 1, cuts, dp));
+        }
+        return dp[i][j] = mini;
+    }
+
+    int minCost(int n, vector<int> &cuts)
+    {
+        int cs = cuts.size();
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>> dp(cs + 1, vector<int>(cs + 1, -1));
+        return find(1, cs, cuts, dp);
+    }
+};
