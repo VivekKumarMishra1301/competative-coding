b9c85ee982d6a14790aebfdcebb033ceee659419
Vivek <vivekkmishra1301@gmail.com>
2023-08-21 14:29:34 +0530

    count maximum possible concatenation and do if not found return -1

---
+++ Leetcode Problems/String_Matching_Repeated_string_count.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int repeatedStringMatch(string a, string b)
+    {
+        int n = b.size() / a.size();
+        int cnt = 1;
+        string k = a;
+        for (int i = 0; i < n + 2; i++)
+        {
+            if (a.find(b) != string::npos)
+            {
+                return cnt;
+            }
+            a += k;
+            cnt++;
+        }
+
+        return -1;
+    }
+};
