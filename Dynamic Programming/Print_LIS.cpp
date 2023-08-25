06de29d4af69f276d7c1e5d4ee02ea82ec6a2871
Vivek <vivekkmishra1301@gmail.com>
2023-08-25 20:57:44 +0530

    using a extra memory to store the track of lis

---
+++ Dynamic Programming/Print_LIS.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<int> dp(nums.size(), 1), hash(nums.size());
+        int maxi = 1;
+        int lastIndex = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            hash[i] = i;
+            for (int prev = 0; prev < i; prev++)
+            {
+                if (nums[i] > nums[prev] && dp[i] < dp[prev] + 1)
+                {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
+            // maxi=max(maxi,dp[i]);
+            if (dp[i] > maxi)
+            {
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        vector<int> lis;
+        lis.push_back(nums[lastIndex]);
+        while (hash[lastIndex] != lastIndex)
+        {
+            lastIndex = hash[lastIndex];
+            lis.push_back(nums[lastIndex]);
+        }
+        reverse(lis.begin(), lis.end());
+        for (int i = 0; i < lis.size(); i++)
+        {
+            cout << lis[i] << " ";
+        }
+        return lis.size();
+    }
+};
