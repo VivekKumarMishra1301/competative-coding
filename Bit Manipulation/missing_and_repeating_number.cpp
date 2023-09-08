541954d0278fe41085016efac8334b811ec22db9
Vivek <vivekkmishra1301@gmail.com>
2023-09-08 12:06:00 +0530

    much optimized

--- Bit Manipulation/missing_and_repeating_number.cpp
+++ Bit Manipulation/missing_and_repeating_number.cpp
@@ -56,3 +56,82 @@ vector<int> findRepeatingAndMissingNumbers(vector<int> nums)
     else
         return {y, x};
 }
+// BEST SOLUTION
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int t, n;
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+
+        int ar[n + 1];
+
+        for (int i = 1; i <= n; i++)
+            cin >> ar[i];
+
+        int resultXor = 0;
+
+        for (int i = 1; i <= n; i++)
+            resultXor = resultXor ^ ar[i];
+        for (int i = 1; i <= n; i++)
+            resultXor = resultXor ^ i;
+
+        int setBit = resultXor & ~(resultXor - 1);
+
+        int x = 0, y = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (ar[i] & setBit)
+            {
+                x = x ^ ar[i];
+            }
+            else
+            {
+                y = y ^ ar[i];
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i & setBit)
+            {
+                x = x ^ i;
+            }
+            else
+            {
+                y = y ^ i;
+            }
+        }
+
+        bool isXMissing = true;
+        for (int i = 1; i <= ar[i]; i++)
+        {
+            if (ar[i] == x)
+            {
+                isXMissing = false;
+                break;
+            }
+        }
+
+        if (isXMissing == true)
+            cout << "Missing Number = " << x << " repeating number = " << y << endl;
+        else
+            cout << "Missing Number = " << y << " repeating number = " << x << endl;
+    }
+}
+
+/*
+n          = 101100
+n - 1      = 101011
+~(n-1)     = 010100
+n & ~(n-1) = 000100
+
+*/
