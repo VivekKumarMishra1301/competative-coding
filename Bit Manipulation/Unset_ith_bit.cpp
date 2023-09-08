7bc9ed2fa11389f4a07fa290ad6192f5a33bdb37
Vivek <vivekkmishra1301@gmail.com>
2023-09-08 12:53:43 +0530

    use xor operation

---
+++ Bit Manipulation/Unset_ith_bit.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // write your code here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int bit;
+        cin >> bit;
+        if ((n & (1 << bit)) != 0)
+            cout << (n ^ (1 << bit)) << endl;
+        else
+        {
+            cout << n << endl;
+        }
+    }
+    return 0;
+}
