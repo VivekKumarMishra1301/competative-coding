ff50fe2db10645c542bab75dd4d9b0431197d042
Vivek <vivekkmishra1301@gmail.com>
2023-10-06 12:02:31 +0530

    use dynamic Programming approach

---
+++ Leetcode Problems/Dynamic_Programming_Integers_Break.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int memo(int n,vector<int>&dp,bool flag){
+       if(n==1||n==0){
+           return 1;
+       }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int k=n;
+        if(flag){
+            k--;
+        }
+        int maxi=INT_MIN;
+        while(k){
+            maxi=max(maxi,k*memo(n-k,dp,false));
+            k--;
+        }
+        return dp[n]=maxi;
+        
+    }
+    
+    
+    int integerBreak(int n) {
+        vector<int>dp(58,-1);
+        return memo(n,dp,true);
+    }
+};
