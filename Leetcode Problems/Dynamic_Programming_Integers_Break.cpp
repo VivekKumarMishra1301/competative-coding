5c357021ff945aa760a7570f06cb583255177bc9
Vivek <vivekkmishra1301@gmail.com>
2023-10-06 12:10:10 +0530

    indentation

--- Leetcode Problems/Dynamic_Programming_Integers_Break.cpp
+++ Leetcode Problems/Dynamic_Programming_Integers_Break.cpp
@@ -1,29 +1,47 @@
-class Solution {
+class Solution
+{
 public:
-    int memo(int n,vector<int>&dp,bool flag){
-       if(n==1||n==0){
-           return 1;
-       }
-        
-        if(dp[n]!=-1){
+    int memo(int n, vector<int> &dp, bool flag)
+    {
+
+        if (n == 1 || n == 0)
+        {
+
+            return 1;
+        }
+
+        if (dp[n] != -1)
+        {
+
             return dp[n];
         }
-        int k=n;
-        if(flag){
+
+        int k = n;
+
+        if (flag)
+        {
+
             k--;
         }
-        int maxi=INT_MIN;
-        while(k){
-            maxi=max(maxi,k*memo(n-k,dp,false));
+
+        int maxi = INT_MIN;
+
+        while (k)
+        {
+
+            maxi = max(maxi, k * memo(n - k, dp, false));
+
             k--;
         }
-        return dp[n]=maxi;
-        
+
+        return dp[n] = maxi;
     }
-    
-    
-    int integerBreak(int n) {
-        vector<int>dp(58,-1);
-        return memo(n,dp,true);
+
+    int integerBreak(int n)
+    {
+
+        vector<int> dp(58, -1);
+
+        return memo(n, dp, true);
     }
 };
